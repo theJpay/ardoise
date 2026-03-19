bdce9c8db799da8982235bd4e87645989bce460a
Jules Paris <jules.paris@360learning.com>
2026-03-19 17:14:17 +0100

    feat: Add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@hooks": path.resolve(__dirname, "./src/hooks"),
+            "@entities": path.resolve(__dirname, "./src/entities/index.ts"),
+            "@entities/*": path.resolve(__dirname, "./src/entities/*"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@services": path.resolve(__dirname, "./src/services")
+        }
+    }
 });
