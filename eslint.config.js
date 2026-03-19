ba4df3beaa8a4ec5dc8e9eeb6c3d912d1afc15c5
Jules Paris <jules.paris@360learning.com>
2026-03-19 11:42:50 +0100

    tooling: format and lint all files before starting development

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import js from '@eslint/js'
-import prettier from 'eslint-config-prettier'
-import prettierPlugin from 'eslint-plugin-prettier'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-    globalIgnores(['dist']),
+    globalIgnores(["dist"]),
     {
-        files: ['**/*.{ts,tsx}'],
+        files: ["**/*.{ts,tsx}"],
         extends: [
             js.configs.recommended,
             tseslint.configs.recommended,
@@ -32,4 +32,4 @@ export default defineConfig([
             "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }]
         }
     }
-])
+]);
