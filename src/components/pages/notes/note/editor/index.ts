1e610fed6ad634fb3de3e47555cc214bf70e6cfd
Jules Paris <jules.paris@360learning.com>
2026-04-27 16:42:49 +0200

    chore: delete legacy editor folder superseded by src/editor

--- src/components/pages/notes/note/editor/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as FloatingToolbar } from "./FloatingToolbar";
-export { default as NoteEditor } from "./NoteEditor";
-export { default as SlashMenu } from "./SlashMenu";
-export { default as Toolbar } from "./Toolbar";
-export { handleFormattingShortcut } from "./handleFormattingShortcut";
-export { useEditorCommands } from "./useEditorCommands";
-export { useEditorEngine } from "./useEditorEngine";
-export { useSlashMenu } from "./useSlashMenu";
-export { useSmartKeys } from "./useSmartKeys";
